a1608c67dbcba896ebba9cdc29d0b37f98841a54
penyelamatbumi <inihanip7@gmail.com>
2026-04-02 16:02:57 +0800

    menambahkan cout

--- mergesort.cpp
+++ mergesort.cpp
@@ -253,3 +253,77 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
+
+    // step 3
+    // fungsi rekursif - memanggil diri sendiri
+    mergeSort(low, mid);      // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
+
+    // step 4
+    int i = low; // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low; // step 4.c
+
+    while (i <= mid && j <= high) // step 4.d
+    {
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+        B[k] = arr[i];
+        i++;
+        }
+        else
+        {
+        B[k] = arr[j];
+        j++;
+        }
+        k++; // step 4.d.ii
+    }
+
+    while (j <= high) // step 4.e
+    {
+        B[k] = arr[j]; // step 4.e.i
+        j++;
+        k++; // step 4.e.ii
+    }
+
+    while (i <= mid) // step 4.f
+    {
+    B[k] = arr[i]; // step 4.f.e.i
+    i++;
+    k++; // step 4.f.ii
+    }
+
+    //step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x]=B[x];
+    }
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+    cout << arr[i]; // step 4.f.i
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
+}
